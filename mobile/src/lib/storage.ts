c95ce84fef943fda4d1ae438fbcafc44dcf23555
BalazsAronSzalai <92146356+BalazsAronSzalai@users.noreply.github.com>
2026-07-16 13:50:31 +0200

    Update storage.ts

--- mobile/src/lib/storage.ts
+++ mobile/src/lib/storage.ts
@@ -7,7 +7,7 @@ export const storage = createMMKV({ id: "superapp" })
 const mmkvStorageAdapter = {
   getItem: (name: string) => storage.getString(name) ?? null,
   setItem: (name: string, value: string) => storage.set(name, value),
-  removeItem: (name: string) => storage.delete(name),
+  removeItem: (name: string) => storage.remove(name), // <-- Changed .delete to .remove
 }
 
 export const zustandStorage = createJSONStorage(() => mmkvStorageAdapter)
